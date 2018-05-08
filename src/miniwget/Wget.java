f38029929eef66d22b5341b6135d1bbba64e1fc4
JesusDiaz08 <jesus_kdm@hotmail.com>
2018-05-08 09:55:06 -0500

    download efra's changes

--- src/miniwget/Wget.java
+++ src/miniwget/Wget.java
@@ -4,6 +4,7 @@ public class Wget {
 
     public Wget(){
         System.out.println("Hello world :'v");
+        System.out.println("Hello x2");
     }
 
     public static void main(String[] args){
