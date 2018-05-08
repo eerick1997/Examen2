afa6c324caed63059b0975d2260ca7d80850d414
Erick Efraín <vargas.erick030997@gmail.com>
2018-05-08 09:35:38 -0500

    exam
    exam

---
+++ src/miniwget/Wget.java
@@ -0,0 +1,13 @@
+package miniwget;
+
+public class Wget {
+
+    public Wget(){
+        System.out.println("Hello world :'v");
+    }
+
+    public static void main(String[] args){
+        new Wget();
+    }
+
+}
