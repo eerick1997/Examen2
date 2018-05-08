c634b9030ecd81cae949f92ad22e7087515ad4e5
Erick Efraín <vargas.erick030997@gmail.com>
2018-05-08 10:17:08 -0500

    :v

--- src/miniwget/Wget.java
+++ src/miniwget/Wget.java
@@ -5,6 +5,7 @@ public class Wget {
     public Wget(){
         System.out.println("Hello world :'v");
         System.out.println("Hello x2");
+        System.out.println("She doesn't love you");
     }
 
     public static void main(String[] args){
