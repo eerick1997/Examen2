91a3adbc8e07e2f79221774e7627134e79d1c328
JesusDiaz08 <jesus_kdm@hotmail.com>
2018-05-08 10:20:49 -0500

    :''v

--- src/miniwget/Wget.java
+++ src/miniwget/Wget.java
@@ -6,6 +6,7 @@ public class Wget {
         System.out.println("Hello world :'v");
         System.out.println("Hello x2");
         System.out.println("She doesn't love you");
+        System.err.println(":'v");
     }
 
     public static void main(String[] args){
