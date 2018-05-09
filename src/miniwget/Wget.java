3ac44fe45c5f65b8a727ecf13496447829aecdcb
Erick Efraín <vargas.erick030997@gmail.com>
2018-05-08 19:20:43 -0500

    This update shows how i'ts a web page downloaded.

--- src/miniwget/Wget.java
+++ src/miniwget/Wget.java
@@ -1,17 +1,38 @@
 package miniwget;
 
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Scanner;
+
 public class Wget {
 
-    public Wget(){
-        System.out.println("Hello world :'v");
-        System.out.println("Hello x2");
-        System.out.println("She doesn't love you");
-        System.err.println(":'v");
-        System.err.println(":''''''''''v");
+    public static void downloadPage(){
+        URL url;
+        InputStream inputStream = null;
+        BufferedReader bufferedReader;
+        String line, web_url = "";
+        try{
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Write the URL of a web page");
+            //web_url = scanner.nextLine();
+            url = new URL(web_url);
+            inputStream = url.openStream();
+            bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+            while((line = bufferedReader.readLine()) != null){
+                System.out.println(line);
+            }
+        } catch (Exception e) {
+            System.err.println("An exception has occurred downloading " + web_url);
+        }
     }
 
     public static void main(String[] args){
-        new Wget();
+        Wget wget = new Wget();
+        wget.downloadPage();
     }
 
 }
